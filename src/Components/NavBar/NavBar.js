f802ff639ff92630d0749a27071193a07eaa3281
Glenda Novak <glendanovak.mkt@gmail.com>
2021-11-15 20:44:27 -0300

    agregando navbar

---
+++ src/Components/NavBar/NavBar.js
@@ -0,0 +1,25 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+
+const NavBar = () => {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#Home">NovakWines</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#Comprar">Comprar</Nav.Link>
+            <Nav.Link href="#Nosotros">Nosotros</Nav.Link>
+            
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default NavBar;
